9c802c082ad65a3ac2ecf59240e3b72f783a994b
Sopida Thippayawat <sopida.t@kkumail.com>
2023-12-14 22:14:29 +0700

    Create dotenv and connect with DB

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"github/mtotheesa/go-e-commerce/config"
+	"github/mtotheesa/go-e-commerce/pkg/databases"
+	"os"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig(envPath())
+	db := databases.DbConnect(cfg.Db())
+	defer db.Close()
+
+	fmt.Println(db)
 }
